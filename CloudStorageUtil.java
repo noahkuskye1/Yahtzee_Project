a850a91cd170923a774913d5ba5a9f25a049ef91
noahkuskye1 <111530647+noahkuskye1@users.noreply.github.com>
2024-09-25 19:12:59 +0000

    Update CloudStorageUtil.java

--- CloudStorageUtil.java
+++ CloudStorageUtil.java
@@ -3,17 +3,22 @@ import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 
-public class CloudStorageUtil {
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class CloudStorageUtil extends YahtzeeGame{
     private static String bucketName = "yahtzee_game_bucket";
-    private static String fileName = "YahtzeeGame.java";
-    private static String content = "gameData.txt";
-    public static void main(String[] args)throws Exception{
-        uploadScore();
-    }
-    public void uploadScore(String bucketName, String fileName, String content) {
+    private static String fileName = "gameData.txt";
+    public static void main(String[] args) throws IOException{
         Storage storage = StorageOptions.getDefaultInstance().getService();
-        BlobId blobId = BlobId.of(bucketName, fileName);
-        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
-        storage.create(blobInfo, content.getBytes());
+
+        File file = new File(fileName);
+        if(file.exists()){
+            BlobId blobId = BlobId.of(bucketName, fileName);
+            BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+            byte[] bytes = Files.readAllBytes(Paths.get(fileName));
+            storage.create(blobInfo, bytes);
+        }
     }
 }
