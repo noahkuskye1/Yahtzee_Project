4d246a1d0314bab444ab190389c7e18370a89d34
noahkuskye1 <111530647+noahkuskye1@users.noreply.github.com>
2024-09-26 03:14:24 +0000

    Update CloudStorageUtil.java

--- CloudStorageUtil.java
+++ CloudStorageUtil.java
@@ -1,16 +1,20 @@
+package com.google.cloud;
+
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class CloudStorageUtil extends YahtzeeGame{
     private static String bucketName = "yahtzee_game_bucket";
-    private static String fileName = "gameData.txt";
-    public static void main(String[] args) throws IOException{
+    private static String fileName = "C:\\Users\\noah_kuskye\\Downloads\\Yahtzee_Project\\Yahtzee_Project\\gameData.txt";
+
+    public static void uploadScore() throws IOException{
         Storage storage = StorageOptions.getDefaultInstance().getService();
 
         File file = new File(fileName);
